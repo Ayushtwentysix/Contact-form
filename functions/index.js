4609e97abf60c8f854a20221713535c833031b87
AyushGup26 <gupta.ayush1997@gmail.com>
2019-04-02 18:46:10 +0000

    mention dependencies in index.js

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
+const nodemailer = require('nodemailer');
+const handlebars = require("handlebars");
